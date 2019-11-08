a909c9ed7aa74cc296f031f6228d306f78931fda
CryptoN999 <nicholasgallucci4@gmail.com>
2019-11-07 18:59:13 -0800

    done with mvp

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Charts from "./components/Charts";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import "./styles.scss";
+
+const App = () => {
+  const [coinData, setCoinData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then(res => setCoinData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className="App">
+      <Navbar />
+      <Charts coinData={coinData} />
+    </div>
+  );
+};
+
+export default App;
